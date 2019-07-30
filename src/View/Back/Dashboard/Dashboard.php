410947f3380de5c7901d877543108292eadb272c
Damien Blaison <d.blaison@live.fr>
2019-07-30 09:41:04 +0200

    Update Dashboard.php

--- src/View/Back/Dashboard/Dashboard.php
+++ src/View/Back/Dashboard/Dashboard.php
@@ -6,9 +6,9 @@ class Dashboard
 
     function render($data){
 
-        var_dump($_SERVER['DOCUMENT_ROOT'].'../src/View/Back/Head.php');
+        var_dump(__DIR__.'/../Head.php');
 
-        require_once($_SERVER['DOCUMENT_ROOT'].'../src/View/Back/Head.php');//flag
+        require_once(__DIR__.'/../Head.php');//flag
 
         $render  = '';
         $render .= '<div class="container-fluid" style="padding-left:0px;">';
